43027616300a7d46cc7f18baf4b7e7a7457d9d07
gian <73489092+gian0012@users.noreply.github.com>
2022-02-23 09:03:43 +0100

    exec() method replaced with system method added sudo privileges prefix added escapeshellarg for function parameters added killBot method

--- src/vpsHandler.php
+++ src/vpsHandler.php
@@ -8,37 +8,35 @@
  */
 
 use \skrtdev\NovaGram\Bot;
-use \skrtdev\Telegram\Message;
 
 $output ??= NULL;
 
 Bot::addMethod("whoami", function () {
 
-    exec("whoami",$output);
-    return $output[0];
+    $output = system("whoami");
+    return $output;
 });
 
-Bot::addMethod("reboot", function () {
+Bot::addMethod("reboot", function () { // WARNING: This method will reboot your VPS.
 
-    exec("reboot",$output);
+    $output = system("sudo reboot");
 });
 
 Bot::addMethod("getIP", function () {
 
-    exec("curl ipinfo.io/ip",$output);
-    return ($output[0]);
+    $l = system("curl ipinfo.io/ip");
+    return ($l);
 });
 
 Bot::addMethod("serviceRestart", function ($serviceName) { // try this commands using: $Bot->serviceRestart("mysql");
-
-    exec("systemctl restart $serviceName ",$output);
+    $escapedServiceName = escapeshellarg($serviceName);
+    exec("systemctl restart $escapedServiceName" );
 });
 
 Bot::addMethod("uptime", function () {
 
-    exec("uptime",$output);
-    $params = explode(" ",$output[0])[1];
-    return " $params UTC";
+    $output = exec("uptime");
+    return " $output UTC";
 });
 
 Bot::addMethod("ramUse", function () {
@@ -50,11 +48,22 @@ Bot::addMethod("ramUse", function () {
     return "$t0\n$t1\n$t2";
 });
 
-Bot::addMethod("startBot", function ($sessionName,$DirToFile) { //try this commands using: $Bot->startBot("screensession","/botdir/main.php"
-                                                                // This command just might to work only with NovaGram or cli-based php libraries.
-    exec("screen -d -S $sessionName -m php $DirToFile");
+Bot::addMethod("startBot", function ($sessionName,$DirToFile) { //try this commands using: $Bot->startBot("screensession","/botdir/main.php");
+    // This command just might to work only with NovaGram or cli-based php libraries.
+    $escapedSessionName = escapeshellarg($sessionName);
+    $escapedDirToFile = escapeshellarg($DirToFile);
+    exec("screen -d -S $escapedSessionName -m php $escapedDirToFile");
 
 });
+
+Bot::addMethod("killBot", function ($sessionName) { //try this commands using: $Bot->killBot("screensession");
+    // This command just might to work only with NovaGram or cli-based php libraries.
+    $escapedSessionName = escapeshellarg($sessionName);
+    exec("screen -X -S $escapedSessionName kill");
+
+});
+
+
 
 
 ?>
